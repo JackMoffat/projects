af4029dcbbe26dfaa6aa095eec38892c575e2ce8
jackmoffat <j4moffat@gmail.com>
2022-01-27 14:31:22 -0500

    rust start ch4

--- rust/rustBookCh3/exercises/src/main.rs
+++ rust/rustBookCh3/exercises/src/main.rs
@@ -1,67 +1,54 @@
+use std::io;
+
 fn main() {
-    for idx in 0..11 {
-        generate_lyrics(idx);
+
+    convert_temperature();
+
+}
+
+fn convert_temperature(unit: String) {
+    // current status of this bad boy is that it'll work (ish) if given correct unit at the outset, but will
+    // not if given teh correct unit after an incorrect guess.
+    // this is somehow due to stdin and &mut saving the info of what user_unit is in the loop somehow
+    // will retunr to 
+    const RATIO_DEG_F_PER_C: f32 = 2.12;
+    // displayed instructions
+    loop {
+        // create variable to hold user input
+        let mut user_temperature = String::new();
+        // get user input and ...mutate or shadow it here when it is returned? or, does neither happen because it was made with just String::new(?)
+        println!("temperature?");
+        io::stdin().read_line(&mut user_temperature).expect("expect....?");
+
+        // from looking at guessing_game
+        // weird, adding the trim() was all it took to make this thing work
+        // it makes it look like an int is returned, but the math shows that it is getting it as an f32
+        let user_temperature: f32 = match user_temperature.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        let user_converted_temperature: f32 = if user_unit == "C" { user_temperature * RATIO_DEG_F_PER_C } else {user_temperature / RATIO_DEG_F_PER_C};
+        // i got the {:.3 syntax by searching google, we encountered it in the rust by exampe}
+        println!("input is currently {}\nconverted temperature:{:.3}",user_temperature,user_converted_temperature);
+        break;
     };
 }
 
-fn generate_lyrics(n: i32) {
-    let numbers_brutally_implemented = [
-        "a",
-        "two",
-        "three",
-        "four",
-        "five",
-        "six",
-        "seven",
-        "eight",
-        "nine",
-        "ten",
-        "eleven",
-        "twelve",
-    ];
-    let day_names_brutally_implemented: [&str; 12] = [
-        "first",
-        "second",
-        "third",
-        "fourth",
-        "fifth",
-        "sixth",
-        "seventh",
-        "eight",
-        "ninth",
-        "tenth",
-        "eleventh",
-        "twelfth",
-    ];
-    let day_gift = [
-        "partridge in a pear tree",
-        "turtle doves",
-        "french hens",
-        "calling birds",
-        "golden rings",
-        "geese a-laying",
-        "swans a-swimming",
-        "maids a-milking",
-        "ladies dancing",
-        "lords a-leaping",
-        "pipers piping",
-        "drummers drumming"
-    ];
 
-    // let mut index = 0;
-    use std::convert::TryInto;
-    for idx in 0..n {
-        // why doesn't this variable need to be mutable?
-        let index:usize = idx.try_into().unwrap();
-        let day_name = day_names_brutally_implemented[index];
-        println!("on the {} day of christmas my true love gave to me",day_name);
-        for interior_idx in (0..idx+1).rev() {
-            let interior_index:usize = interior_idx.try_into().unwrap();
-            if index > 0 && interior_index == 0 {
-                println!("and {} {}",numbers_brutally_implemented[interior_index],day_gift[interior_index]);
-            } else {
-                println!("{} {}",numbers_brutally_implemented[interior_index],day_gift[interior_index]);
-            };
-           }
+fn get_unit() {
+    let mut user_unit = String::new();
+    let user_unit = loop {
+        println!("Celsius(C) or fahrenheit?(F)");
+        io::stdin().read_line(&mut user_unit).expect("expect....?");
+        println!("user unit {}",user_unit);
+        let user_unit_internal = user_unit.trim();
+        if user_unit_internal == "C" || user_unit_internal == "F" {
+            println!("user unit internal success is {}", user_unit_internal);
+            break user_unit_internal; }
+        else {
+            println!("try again ya dingus");
+            continue;
         };
-    }
+
+    };
+}
