1ef835374631112b5e976931b9c53dad23787d7a
jackmoffat <j4moffat@gmail.com>
2022-02-14 08:22:46 -0500

    prep move 2

---
+++ workoutAppTests/Juggernaut/5sWaveMainParams.py
@@ -0,0 +1,23 @@
+
+
+Accumulation = {
+	"Intensity": 0.7,
+	"Sets": 6,
+	"Reps": 5,
+	}
+
+
+Intensification = {
+	"Intensity": "0.65,0.725,0.775",
+	"Sets": "1,1,4",
+	"Reps":"2,2,5"
+	}
+	
+	
+Realization = {
+	"Intensity": "0.5,0.6,0.7,0.75,0.8,0.85",
+	"Sets": "1,1,1,1,1,1",
+	"Reps": "1,1,1,1,1,max"
+	}
+	
+
---
+++ workoutAppTests/Juggernaut/ExerciseDicts.py
@@ -0,0 +1,12 @@
+BenchDay = {
+	"Bench" : [['mains']],
+	"Horizontal Pull" : [['3-5'],['6-12'],[3]],
+	"Dips or Triceps focused press" : [[3],['max'],[3]],
+	"Ab Wheel" : [['3-5'],['15-20'],[2]],
+	"Smith Machine Throw" : [[10],[2],[1.5]],
+
+
+}
+
+
+print(BenchDay['Bench'])
