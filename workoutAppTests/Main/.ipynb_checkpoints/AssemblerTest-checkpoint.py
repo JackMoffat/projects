db9a85588d005bd995a5b0df857e4be30f7c6943
jackmoffat <j4moffat@gmail.com>
2022-02-15 14:05:51 -0500

    adds

--- workoutAppTests/Main/.ipynb_checkpoints/AssemblerTest-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import Classes
-import FourDayExercises
-
-plan = Classes.Macrocycle()
-plan.meso_dir()
-
-import kivy
-from kivy.uix.textinput import TextInput
-textinput = TextInput(text='Hello world')
