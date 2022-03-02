901ab3aacac571db88649765230462f24061fd18
jackmoffat <j4moffat@gmail.com>
2022-03-02 11:35:55 -0500

    adds

--- rust/rustJuggernaut/src/main.rs
+++ rust/rustJuggernaut/src/main.rs
@@ -1,3 +1,17 @@
+
+
 fn main() {
     println!("Hello, world!");
+}
+
+
+// structs can hold or reference enum types!!!
+// holds information for each exercise that will be displayed initially?
+// how will it take in user data
+struct Exercise {
+    name : String,
+    sets: usize,
+    reps: usize,
+    rest: isize,
+
 }
