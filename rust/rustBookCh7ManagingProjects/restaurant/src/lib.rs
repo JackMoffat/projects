c07777cb6b037ad48faecf1281c1216e31d9d46f
jackmoffat <j4moffat@gmail.com>
2022-03-29 09:38:10 -0700

    chapter 7.4-7.5

--- rust/rustBookCh7ManagingProjects/restaurant/src/lib.rs
+++ rust/rustBookCh7ManagingProjects/restaurant/src/lib.rs
@@ -1,23 +1,9 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+mod front_of_house;
 
-mod front_of_house {
-    fn add_to_waitlist() {}
+pub use crate::front_of_house::hosting
 
-    fn seat_at_table() {}
-
-}
-
-mod serving {
-    fn take_order() {}
-
-    fn serve_order() {}
-
-    fn take_payment() {}
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 }
