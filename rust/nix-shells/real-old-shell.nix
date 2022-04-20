c473b801a85eaf54de8fec90e84fb0e1f3e643b0
jackmoffat <j4moffat@gmail.com>
2022-04-20 11:07:15 -0700

    ok, real-old-shell.nix appears to be working now
    One of the issues, at least the one that was breaking the build, was an existing
    reference to libvmi that was present in the "RUSTFLAGS" section.
    removal of this reference, at least after all potentially virtualization-related
    packages commented out, was enough to get the shell to build again.

--- rust/nix-shells/real-old-shell.nix
+++ rust/nix-shells/real-old-shell.nix
@@ -1,23 +1,23 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell rec {
     buildInputs = with pkgs; [
-      rustc
-      cargo
       llvmPackages_latest.llvm
       llvmPackages_latest.bintools
-      zlib.out
+      rustc
+      cargo
+      # zlib.out
       rustup
-      xorriso
-      grub2
+      # xorriso
+      # grub2
       #qemu
-      llvmPackages_latest.lld
+      # llvmPackages_latest.lld
       python3
     ];
 
     RUSTC_VERSION = pkgs.lib.readFile ./rust-toolchain;
     # https://github.com/rust-lang/rust-bindgen#environment-variables
     LIBCLANG_PATH= pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
-    HISTFILE=toString ./.history;
+    # HISTFILE=toString ./.history;
     # I added the export cargo line below
     # the export PATH should fail. but wait, now I do have a .cargo?
     # for some reason, I now *do* have a ~/.cargo directory. 
@@ -30,15 +30,10 @@
       export PATH=$PATH:~/.cargo/bin
       export PATH=$PATH:~/.rustup/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
       '';
-    # Add libvmi precompiled library to rustc search path
-    RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
-      pkgs.libvmi
-    ]);
     # Add libvmi, glibc, clang, glib headers to bindgen search path
     BINDGEN_EXTRA_CLANG_ARGS = 
     # Includes with normal include path
     (builtins.map (a: ''-I"${a}/include"'') [
-      pkgs.libvmi
       pkgs.glibc.dev
     ])
     # Includes with special directory paths
