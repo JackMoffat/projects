01b07fd56f10378e8d995fae3f1e3800c43f8402
jackmoffat <j4moffat@gmail.com>
2022-04-21 10:01:30 -0700

    adds

--- rust/rustBookCh8-Collections-Strings-Vectors-HashMaps/ch8-exercises/src/main.rs
+++ rust/rustBookCh8-Collections-Strings-Vectors-HashMaps/ch8-exercises/src/main.rs
@@ -1,12 +1,12 @@
 // in rust this would be an array
 fn main() {
-list = [20, 6, 2, 1, 16, 14, 2, 2, 19, 5, 9, 4, 1, 10, 3, 1, 12, 2, 7, 10, 17, 14, 3, 5, 10, 7, 1, 8, 7, 16, 5, 0, 12, 3, 0, 18, 6, 6, 8, 3];
+    list = [20, 6, 2, 1, 16, 14, 2, 2, 19, 5, 9, 4, 1, 10, 3, 1, 12, 2, 7, 10, 17, 14, 3, 5, 10, 7, 1, 8, 7, 16, 5, 0, 12, 3, 0, 18, 6, 6, 8, 3];
 
-let v: Vec<i32> = Vec::new();
+    let v: Vec<i32> = Vec::new();
 
-for elem in list {
-    v.push(elem);
-}
+    for elem in list {
+        v.push(elem);
+    }
 
-println!("{:?}",v);
+    println!("{:?}",v);
 }
